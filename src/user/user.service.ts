1d3f6981dfd926a7c679362046f43efa65522dd8
João Pedro Thuler Lima <thuler_lima@hotmail.com>
2023-11-30 17:33:05 -0300

    feat: implemented bcrypt while creating user password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,17 +4,32 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { handleError } from 'src/utils/handleError';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
+  private userSelect = {
+    id: true,
+    name: true,
+    email: true,
+    password: false,
+    createdAt: true,
+    updatedAt: true,
+  };
+
   constructor(private readonly prisma: PrismaService) {}
 
   findAll() {
-    return this.prisma.user.findMany();
+    return this.prisma.user.findMany({
+      select: this.userSelect,
+    });
   }
 
   async findById(id: string): Promise<User> {
-    const data = await this.prisma.user.findUnique({ where: { id } });
+    const data = await this.prisma.user.findUnique({
+      where: { id },
+      select: this.userSelect,
+    });
     if (!data) {
       throw new BadRequestException(`Unavailable ID`);
     }
@@ -22,9 +37,17 @@ export class UserService {
   }
 
   async create(dto: CreateUserDto): Promise<User> {
-    const data: User = { ...dto };
-    await this.prisma.user.create({ data }).catch(handleError);
-    return data;
+    if (dto.confirmPassword !== dto.password) {
+      throw new BadRequestException(`Passwords do not march`);
+    }
+    delete dto.confirmPassword;
+    const data: User = {
+      ...dto,
+      password: await bcrypt.hash(dto.password, 10),
+    };
+    return this.prisma.user
+      .create({ data, select: this.userSelect })
+      .catch(handleError);
   }
 
   async delete(id: string): Promise<void> {
@@ -38,6 +61,7 @@ export class UserService {
     return this.prisma.user.update({
       where: { id },
       data,
+      select: this.userSelect,
     });
   }
 }
